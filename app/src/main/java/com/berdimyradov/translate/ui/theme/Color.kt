cdacae1a3380c1768f25e5b8b1c785f789bd90ec
iazat6393@gmail.com <iazat6393@gmail.com>
2024-02-01 22:17:08 +0500

    init project

---
+++ app/src/main/java/com/berdimyradov/translate/ui/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.berdimyradov.translate.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryLight = Color(0xFF006a66)
+val secondaryLight = Color(0xFF454c4c)
+val tertiaryLight = Color(0xFF90f3ee)
+val backgroundLight = Color(0xFFf0fcfa)
+val surfaceLight = Color(0xFFe4f4f1)
+val onPrimaryLight = Color(0xFFcbe8e4)
+val onSecondaryLight = Color(0xFFe5efee)
+val onTertiaryLight = Color.White
+val onBackgroundLight = Color(0xFF1C1B1F)
+val onSurfaceLight = Color(0xFF1C1B1F)
+
+val primaryDark = Color(0xFF006a66)
+val secondaryDark = Color(0xFF454c4c)
+val tertiaryDark = Color(0xFF90f3ee)
+val backgroundDark = Color(0xFFf0fcfa)
+val surfaceDark = Color(0xFFe4f4f1)
+val onPrimaryDark = Color(0xFFcbe8e4)
+val onSecondaryDark = Color(0xFFe5efee)
+val onTertiaryDark = Color.White
+val onBackgroundDark = Color(0xFF1C1B1F)
+val onSurfaceDark = Color(0xFF1C1B1F)
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GoogleTranslate Clone"
+include(":app")
+ 
